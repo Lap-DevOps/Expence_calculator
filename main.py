0f736e203bd905035ed8b3223fabdc980a7c2992
AndriiLevin <1@1.ru>
2023-01-08 20:10:48 +0000

    Init and make main window and three frames with ins location

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-def print_out(a):
-    print(a)
-    print(a * 2)
+# This is example of Python program.
+# Small test Python program to work with TKInter as desktop app and SQLite.
 
+import tkinter as tk
+
+# initiate main window and app name
+main_window = tk.Tk()
+main_window.title("My App")
+
+# make several frames in window and locate them
+frame_add_form = tk.Frame(main_window, width=150, height=150, bg="green")
+frame_statistic = tk.Frame(main_window, width=150, height=150, bg="yellow")
+frame_list = tk.Frame(main_window, width=300, height=150, bg="red")
+frame_add_form.grid(row=0, column=0)
+frame_statistic.grid(row=0, column=1)
+frame_list.grid(row=1, column=0, columnspan=2)
 
 if __name__ == '__main__':
-    print_out("Hello world!")
+    main_window.mainloop()
