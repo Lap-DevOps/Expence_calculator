1257412d615b55c777cf22e55d3430705fdeea37
AndriiLevin <1@1.ru>
2023-01-11 21:33:44 +0000

    TIK-002: -deleted unused command

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ import expenses_helper as eh
 # initiate main window and app name
 main_window = tk.Tk()
 main_window.title("My App")
-# main_window.geometry("500x500")
-# main_window.minsize(250, 250)
-# main_window.maxsize(500, 500)
 
 # make several frames in window and locate them
 frame_add_form = tk.Frame(main_window, bg="green")
@@ -45,10 +42,10 @@ l_exp_month_value.grid(row=3, column=1, sticky='e', padx=10, pady=10)
 
 # filling list grid with table and scroll
 
+
 heads = ["id", "name", "amount", "date"]
 table = ttk.Treeview(frame_list, show="headings")
 table["columns"] = heads
-#table["displaycolumns"] = ["", "", "", ""]
 
 for header in heads:
     table.heading(header, text=header, anchor="center")
@@ -65,9 +62,8 @@ table.pack(expand=tk.YES, fill=tk.BOTH)
 
 # labels for forms
 l_temp_frame_add_form = tk.Label(frame_add_form, text="Add frame form")
-#l_temp_frame_list = tk.Label(frame_list, text="Statistic")
 l_temp_frame_add_form.pack(expand=True, padx=20, pady=20)
-#l_temp_frame_list.pack(expand=True, padx=20, pady=20)
 
+# main loop
 if __name__ == '__main__':
     main_window.mainloop()
